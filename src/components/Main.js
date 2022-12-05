4fa3baf2b0d09583c53254ff473978f76002056e
theparthshira <shirawalaparth@gmail.com>
2022-12-05 16:31:54 +0530

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,145 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div class="container card">
+      <div class="container-row1 clearfix">
+        <div class="row1-col1 green">
+          <div class="row1-col1-child clearfix white">
+            <div class="green">GG</div>
+            <div class="green">GG</div>
+            <div class="green">GG</div>
+            <div class="green">GG</div>
+          </div>
+        </div>
+        <div class="row1-col2 clearfix">
+          <div>11</div>
+          <div>12</div>
+          <div>13</div>
+          <div>10</div>
+          <div class="yellow"></div>
+          <div class="yellow">14</div>
+          <div>9</div>
+          <div class="yellow"></div>
+          <div>15</div>
+          <div>8</div>
+          <div class="yellow"></div>
+          <div>16</div>
+          <div>7</div>
+          <div class="yellow"></div>
+          <div>17</div>
+          <div>6</div>
+          <div class="yellow"></div>
+          <div>18</div>
+        </div>
+        <div class="row1-col3 yellow">
+          <div class="row1-col3-child clearfix white">
+            <div class="yellow">YY</div>
+            <div class="yellow">YY</div>
+            <div class="yellow">YY</div>
+            <div class="yellow">YY</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-row2 clearfix">
+        <div class="row2-col1 clearfix">
+          <div id="52">52</div>
+          <div id="1" class="green">
+            1
+          </div>
+          <div id="2">2</div>
+          <div id="3">3</div>
+          <div id="4">4</div>
+          <div id="5">5</div>
+          <div id="51">51</div>
+          <div id="" class="green"></div>
+          <div id="" class="green"></div>
+          <div id="" class="green"></div>
+          <div id="" class="green"></div>
+          <div id="" class="green"></div>
+          <div id="50">50</div>
+          <div id="49">49</div>
+          <div id="48">48</div>
+          <div id="47">47</div>
+          <div id="46">46</div>
+          <div id="45">45</div>
+        </div>
+        <div class="row2-col2">
+          <div class="contain-triangles">
+            <div class="triangle-right"></div>
+            <div class="triangle-up"></div>
+            <div class="triangle-left"></div>
+            <div class="triangle-down"></div>
+          </div>
+        </div>
+        <div class="row2-col3 clearfix">
+          <div id="19">19</div>
+          <div id="20">20</div>
+          <div id="21">21</div>
+          <div id="22">22</div>
+          <div id="23">23</div>
+          <div id="24">24</div>
+          <div id="" class="blue"></div>
+          <div id="" class="blue"></div>
+          <div id="" class="blue"></div>
+          <div id="" class="blue"></div>
+          <div id="" class="blue"></div>
+          <div id="25">25</div>
+          <div id="31">31</div>
+          <div id="30">30</div>
+          <div id="29">29</div>
+          <div id="28">28</div>
+          <div id="27" class="blue">
+            27
+          </div>
+          <div id="26">26</div>
+        </div>
+      </div>
+
+      <div class="container-row3 clearfix">
+        <div class="row3-col1 red">
+          <div class="row3-col1-child clearfix white">
+            <div class="red">RR</div>
+            <div class="red">RR</div>
+            <div class="red">RR</div>
+            <div class="red">RR</div>
+          </div>
+        </div>
+        <div class="row3-col2 clearfix">
+          <div id="44">44</div>
+          <div id="" class="red"></div>
+          <div id="32">32</div>
+          <div id="43">43</div>
+          <div id="" class="red"></div>
+          <div id="33">33</div>
+          <div id="42">42</div>
+          <div id="" class="red"></div>
+          <div id="34">34</div>
+          <div id="41">41</div>
+          <div id="" class="red"></div>
+          <div id="35">35</div>
+          <div id="40" class="red">
+            40
+          </div>
+          <div id="" class="red"></div>
+          <div id="36">36</div>
+          <div id="39">39</div>
+          <div id="38">38</div>
+          <div id="37">37</div>
+        </div>
+        <div class="row3-col3 blue">
+          <div class="row3-col3-child clearfix white">
+            <div class="blue">BB</div>
+            <div class="blue">BB</div>
+            <div class="blue">BB</div>
+            <div class="blue">BB</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
